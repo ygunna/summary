e5720a6c15671b46dc82935dc63a00ba17703360
ygunna <rgy125@naver.com>
2021-01-18 00:26:40 +0900

    p31

---
+++ Java_정석_3rd/com/hello01/Hello.java
@@ -0,0 +1,7 @@
+package com.hello01;
+
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
---
+++ Java_정석_3rd/java_basic/3rd/com/hello01/Hello.java
@@ -0,0 +1,10 @@
+package com.hello01;
+
+class Hello {
+    public static void main(String[] args) {
+
+        int year = 2020;
+        final int num = 100;
+        System.out.println("Hello, world" + num);
+    }
+}
