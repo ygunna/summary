d377b0a2c7c7167a40a8169df986e3c2cb05584d
ygunna <rgy125@naver.com>
2021-01-24 00:25:02 +0900

    p191 scanner, constant, literal, array

--- Java_정석_3rd/com/hello01/Hello.java
+++
@@ -1,7 +0,0 @@
-package com.hello01;
-
-class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, world");
-    }
-}
--- Java_정석_3rd/java_basic/3rd/com/hello01/Hello.java
+++ Java_정석_3rd/java_basic/3rd/com/hello01/Hello.java
@@ -1,10 +1,40 @@
 package com.hello01;
 
+import java.util.Scanner;
+
 class Hello {
     public static void main(String[] args) {
 
         int year = 2020;
-        final int num = 100;
-        System.out.println("Hello, world" + num);
+        final long FINAL_LONG;
+        final float FINAL_FLOAT = 3.14f;
+        String str = "abc";
+        char ch = 'a';
+        String input;
+        int input_num = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[4];
+        
+        // Constant & Literal
+        try {
+            FINAL_LONG = 200L;
+            input = scanner.nextLine();
+            System.out.println("Input Please\n");
+            input_num = Integer.parseInt(input);
+            // FINAL_NUM = 300; java: variable FINAL_NUM might already have been assigned
+            // FINAL_FLOAT = 1.14f; java: variable FINAL_NUM might already have been assigned
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = i;
+            }
+
+            for (int j = 0; j < arr.length; j++) {
+                System.out.printf("arr[%d] = %d\n", j, arr[j]);
+            }
+        } finally {
+            System.out.println("input = " + input_num);
+        }
+        System.out.println("Hello, world " + FINAL_LONG + " "
+                + " " + FINAL_FLOAT);
     }
 }
